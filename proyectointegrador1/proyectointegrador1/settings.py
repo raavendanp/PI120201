bfba6e9b015638b784cd8fb4f2e7129d5b0edb15
unknown <raavendanp@eafit.edu.co>
2020-05-26 14:39:04 -0500

    modificando settings

--- proyectointegrador1/proyectointegrador1/settings.py
+++ proyectointegrador1/proyectointegrador1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h&%a%@&-28837d_d2#pvft@csxur1bcfaq&e%a@5t-kp!%ucm7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
